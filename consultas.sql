9288a000d70a9633a947ab6b9be30cd9064a0dba
Williams Hernández <hernandw@gmail.com>
2026-02-25 19:22:38 -0300

    :sparkles: inserción de registros

--- consultas.sql
+++ consultas.sql
@@ -16,3 +16,25 @@ clave varchar(60)
 select * from usuarios;
 
 drop table if exists usuarios;
+
+/* INSERCIÓN DE REGISTROS */
+
+--forma1
+INSERT INTO usuarios (nombre, clave) values('Arly', '123456');
+INSERT INTO usuarios (nombre, clave) values('Juan', '12345678');
+INSERT INTO usuarios (nombre, clave) values('David', '123456AB');
+INSERT INTO usuarios (nombre, clave) values('Walter', '12345678ABC');
+
+--forma2
+INSERT INTO usuarios 
+values('Jorge', 'asdfg');
+
+
+--forma3
+INSERT INTO usuarios (nombre, clave) 
+values 
+('Juan Emilio', '123456'),
+('Yolanda', '123456'),
+('Josefa', '123456'),
+('Javiera', '123456');
+
